8aec9836291a5c1c90d65ca00afb8a334da47ec7
Emmaline Salles <emmalinesalles@Emmalines-MacBook-Air.local>
2017-08-29 09:06:35 -0400

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,71 @@
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.floor      = 0; //floor where elevator currently is
+    this.MAXFLOOR   = 10; //top floor of the building (max value of "this.floor")
+    this.direction = 'up'; //current diraction that the elevator is moving in. ('up' or 'down')
+    this.requests   = []; //array of numbers of the floors which elevator must stop. can be people waiting for elevator or those on the elevator
+    this.waitingList = []; //array of person objects that represent people waiting to be picked up
+    this.passengers = []; //array of person objects that represents people on the elevator waiting to be dropped off
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {  //begin a set interval that calls "update()" every second
+    this.cow = setInterval(() => {
+      this.update();
+    }, 1000);
+  }
+  stop() { //cancel set interval by start.
+    clearInterval(this.cow);
+  }
+
+  update() { //for now, will only call "log()"
+      this.log();
+  }
+
+  log() {
+    //displays the direction and current floor
+    var direction = `direction: ${this.direction} | floor: ${this.floor}`;
+    console.log(direction);
+  }
+
+  _passengersLeave(myPerson) {
+      this.passengers.push(myPerson);
+      this.requests.push(myPerson.destinationFloor);
+
+      let indexOfPerson = this.passengers.indexOf(myPerson);
+      if (indexOfPerson > -1) {
+        console.log(`${myPerson.name} has left the elevator`);
+        this.passengers.splice(indexOfPerson, 1);
+      }
+  }
+
+  _passengersEnter(myPerson) {
+      this.passengers.forEach ()
+  }
+
+  floorUp() {
+    if (this.floor < 10) {
+      this.floor +=1;
+      this.log();
+    } else {
+    this.log();
+    }
+  }
+
+  floorDown() {
+    if (this.floor >= 0) {
+      this.floor -=1;
+      this.log();
+    } else {
+    this.log();
+    }
+  }
+
+  call(myPerson) {
+    this.waitingList.push(myPerson);
+    this.requests.push(myPerson.originFloor);
+  }
+
+
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,25 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+
+
+const myElevator = new Elevator();
+const myPerson = new Person('Emma', 1, 6);
+
+
+//starts the elevator
+
+// console.log(myElevator);
+myElevator.update();
+myElevator.floorUp();
+myElevator.floorDown();
+myElevator.call(myPerson);
+console.log('hi');
+
+
+
+
+
+
+
+//For iteration 2: hardcode "floorUp()" and "floorDown()"
